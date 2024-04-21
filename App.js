dfe146cdbe28b69ab38aef5368f3a309f0bedc3f
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-04-21 17:38:58 +0100

    depencies resolved

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ import SignInScreen from './components/Home/SignInScreen';
 import SignUpScreen from './components/Home/SignUpScreen';
 import EditProfileScreen from './components/Home/EditProfileScreen';
 import PostDetailsScreen from './components/Home/PostDetailsScreen';
-import OtherProfileScreen from './components/Home/OtherProfileScreen';
+// import OtherProfileScreen from './components/Home/OtherProfileScreen';
 import ChatScreen from './components/Home/ChatScreen';
 
 const Stack = createStackNavigator();
